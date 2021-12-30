d50a2395f0ee4b44423f6c09495e15ec0d64b5dd
Artem Rzakuleev <ArtemRzakuleev@gmail.com>
2021-12-30 13:08:45 +0400

    add calculator of number from CLI

---
+++ src/main/java/com/epam/training/student_artem_rzakuleev/fundamental/main_task/Calculator.java
@@ -0,0 +1,14 @@
+package com.epam.training.student_artem_rzakuleev.fundamental.main_task;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length==2) {
+            int a=Integer.valueOf(args[0]);
+            int b=Integer.valueOf(args[1]);
+            System.out.println(a+" + "+b+" = "+(a+b));
+            System.out.println(a+" * "+b+" = "+(a*b));
+        }
+    }
+}
