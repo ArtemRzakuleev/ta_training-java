482b0c3facbfd91b6dc6f7f528ab40b91e52a930
Artem Rzakuleev <ArtemRzakuleev@gmail.com>
2021-12-30 12:56:50 +0400

    change codestyle

--- src/main/java/com/epam/training/student_artem_rzakuleev/fundamental/main_task/ReverseArgs.java
+++ src/main/java/com/epam/training/student_artem_rzakuleev/fundamental/main_task/ReverseArgs.java
@@ -2,10 +2,8 @@ package com.epam.training.student_artem_rzakuleev.fundamental.main_task;
 
 public class ReverseArgs {
     public static void main(String[] args) {
-        if (args.length>0)
-        {
-            for (int i= args.length-1;i>0;i--)
-            {
+        if (args.length>0) {
+            for (int i= args.length-1;i>0;i--) {
                 System.out.print(args[i]+" ");
             }
             System.out.print(args[0]);
