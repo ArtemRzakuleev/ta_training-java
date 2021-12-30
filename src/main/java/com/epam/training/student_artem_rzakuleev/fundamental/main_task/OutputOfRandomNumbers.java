3763837c71266c697a5ae326350efafd66c03384
Artem Rzakuleev <ArtemRzakuleev@gmail.com>
2021-12-30 12:53:47 +0400

    add output of random numbers

--- src/main/java/com/epam/training/student_artem_rzakuleev/fundamental/main_task/OutputOfRandomNumbers.java
+++ src/main/java/com/epam/training/student_artem_rzakuleev/fundamental/main_task/OutputOfRandomNumbers.java
@@ -1,2 +1,23 @@
-package com.epam.training.student_artem_rzakuleev.fundamental.main_task;public class OutputOfRandomNumbers {
+package com.epam.training.student_artem_rzakuleev.fundamental.main_task;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class OutputOfRandomNumbers {
+    public static void main(String[] args) {
+        System.out.print("Enter count of numbers: ");
+        Scanner in = new Scanner(System.in);
+        int count=in.nextInt();
+        System.out.print("Random number without separator: ");
+        Random r=new Random();
+        for (int i=0;i<count;i++)
+        {
+            System.out.print(r.nextInt()+"\t");
+        }
+        System.out.println("\nRandom number with separator");
+        for (int i=0;i<count;i++)
+        {
+            System.out.println(r.nextInt()+"\t");
+        }
+    }
 }
